d4b16d3f6cd560fc8539c36d3330f3ed99be52f0
Torrie Fischer <tdfischer@hackerbots.net>
2013-10-22 18:07:59 -0400

    Explicitly call phong.py sudo

--- phong-su.c
+++ phong-su.c
@@ -9,5 +9,14 @@
 
 int main(int argc, char** argv)
 {
-  return execv (PHONG_PATH, argv);
+  int i;
+  char **newArgv;
+
+  newArgv = calloc (sizeof (char*), argc+1);
+  newArgv[0] = strdup (PHONG_PATH);
+  newArgv[1] = strdup ("sudo");
+  for (i = 1; i < argc; i++) {
+    newArgv[i+1] = strdup(argv[i]);
+  }
+  return execv (PHONG_PATH, newArgv);
 }
