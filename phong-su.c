79a0f23499b0ff6b63403192ec99b47004945fb8
Torrie Fischer <tdfischer@hackerbots.net>
2013-10-24 13:24:00 -0400

    license++

--- phong-su.c
+++ phong-su.c
@@ -1,6 +1,24 @@
 /*
  * A simple setuid binary that runs /usr/bin/phong.py
+ *
+ * Copyright (C) 2013 Torrie Fischer <tdfischer@hackerbots.net>
+ *
+ * This file is part of phong.
+ *
+ * Phong is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Phong is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with phong.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include <sys/types.h>
 #include <unistd.h>
 
