ab0db510e33f3a3d16d932b5b956da2ad215ca8d
Torrie Fischer <tdfischer@hackerbots.net>
2014-09-08 18:09:08 -0700

    Add nb-manual command

--- plugins/noisebridge.py
+++ plugins/noisebridge.py
@@ -22,10 +22,58 @@ import datetime
 import hashlib
 import calendar
 from icalendar import Calendar, Event
+import wikitools.page
 
 class NoisebridgePlugin(Plugin):
   def availableCommands(self):
-    return [NBEvents]
+    return [NBEvents, NBManual]
+
+class NBManual(Command):
+  def name(self):
+    return "nb-wiki-manual"
+
+  def helpText(self):
+    return "Updates the Manual page on the wiki, and tags pages on the manual"
+
+  def execute(self, args):
+    wiki = self.phong.wiki.getPage("Manual").getWikiText()
+    structure = mw.parse(wiki)
+    manualSections = {}
+    for c in structure.get_sections(flat=True):
+      titles = c.filter_headings()
+      if len(titles):
+        sectionPages = []
+        for line in c.ifilter_wikilinks():
+          sectionPages.append(unicode(line.title))
+
+        manualSections[titles[0].title.strip()] = sectionPages
+
+    sectionWiki = self.phong.wiki.getPage("Template:ManualSections")
+    sectionText = ' - '.join(map(lambda x:"[[Manual#%s|%s]]"%(x,x), manualSections.iterkeys()))
+    try:
+      if sectionWiki.getWikiText() != sectionText:
+        sectionWiki.edit(text=sectionText, bot=True)
+    except wikitools.page.NoPage:
+        sectionWiki.edit(text=sectionText, bot=True)
+
+    for sectionName, pages in manualSections.iteritems():
+      for page in pages:
+        print "%s: %s"%(sectionName, page)
+        try:
+          pageWiki = self.phong.wiki.getPage(unicode(line.title)).getWikiText()
+        except wikitools.page.NoPage:
+          print "\t✗ Not even a page!!!!"
+          continue
+        manPage = mw.parse(pageWiki)
+        inManual = False
+        for tpl in manPage.ifilter_templates():
+          if tpl.name == "ManualPage":
+            inManual = True
+            break
+        if inManual:
+          print "\t✓ In manual"
+        else:
+          print "\t✗ Not in manual!"
 
 class NBEvents(Command):
   def name(self):
